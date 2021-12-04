78415a15ea1430585ee7767f776e9da95378e749
Chris Rabe (AU) <chris.rabe14@gmail.com>
2021-12-04 23:50:36 +1000

    feat: added all import functions

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,12 @@
 import dotenv from 'dotenv';
 dotenv.config();
 import { loginToGimmonix } from './client';
-import { importHotelContent } from './importers';
+import {
+  importHotelContent,
+  importHotelDescription,
+  importHotelFacilities,
+  importHotelImages,
+} from './importers';
 
 export const GMX_MAPPING_URL = 'https://live.mapping.works/Mapping';
 
@@ -9,7 +14,17 @@ const importGMXFiles = async () => {
   const cookie = await loginToGimmonix();
   const time = 'Time taken to complete all download operations';
   console.time(time);
+
   await importHotelContent(cookie);
+
+  const importAttributes: Promise<unknown>[] = [
+    importHotelFacilities(cookie),
+    importHotelDescription(cookie),
+    importHotelImages(cookie),
+  ];
+
+  await Promise.all(importAttributes);
+
   console.timeEnd(time);
 };
 
