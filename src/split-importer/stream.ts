07de1f7e4d0119383359bb4852885b7c07985f22
Chris Rabe (AU) <chris.rabe14@gmail.com>
2022-03-03 09:52:33 +1000

    feat: created generic importer

---
+++ src/split-importer/stream.ts
@@ -0,0 +1,50 @@
+import got from 'got';
+import { promisify } from 'util';
+import { Parse } from 'unzip-stream';
+import stream, { Transform, TransformCallback } from 'stream';
+import { chain } from 'stream-chain';
+const etl = require('etl');
+import { FileType, chainFns } from '../stream-importer/stream';
+
+const pipeline = promisify(stream.pipeline);
+
+export const streamData = async <L>(
+  remoteUrl: string,
+  expectedFile: string,
+  fileType: FileType,
+  transformer: Transform,
+  loader: L,
+  maxItems = 1000
+) => {
+  const timer = `Time taken to download ${expectedFile}`;
+  console.time(timer);
+
+  const downloadStream = got.stream(remoteUrl); // input stream
+
+  const filePipeline = chain([
+    ...chainFns[fileType](transformer),
+    etl.collect(maxItems),
+    loader,
+  ]);
+
+  // transforms
+  const zipParser = Parse();
+  const fileParser = new stream.Transform({
+    objectMode: true,
+    transform(chunk: any, _: BufferEncoding, cb: TransformCallback) {
+      if (chunk.path === expectedFile) {
+        chunk.pipe(filePipeline).on('finish', cb);
+      } else {
+        chunk.autodrain();
+        cb();
+      }
+    },
+  });
+
+  try {
+    await pipeline(downloadStream, zipParser, fileParser);
+    console.timeEnd(timer);
+  } catch (e: any) {
+    console.error(`Something went wrong: ${e.message}`);
+  }
+};
