ab7645e281759f6d13346096994616a87a7a3200
Chris Rabe (AU) <chris.rabe14@gmail.com>
2022-03-15 10:06:14 +1000

    feat: updated stream importer

--- src/stream-importer/index.ts
+++ src/stream-importer/index.ts
@@ -4,8 +4,27 @@ import {
   importHotelFacilities,
   importHotelImages,
 } from './importer';
+import { loginToGimmonix } from '../client';
+import { ELASTIC_INDEX, getElasticClient } from '../elastic/client';
+import { createIndex, generateIndexName } from '../elastic/actions';
+import { elasticLoader, ElasticMethod } from './transforms/loader';
 
-export const importer = async <L>(
+export const importer = async () => {
+  const cookie = await loginToGimmonix();
+  const client = getElasticClient();
+
+  const indexName = generateIndexName(ELASTIC_INDEX);
+  await createIndex(client, indexName);
+
+  console.log('Uploading to index', indexName);
+
+  const indexLoader = elasticLoader(ElasticMethod.Index, client, indexName);
+  const updateLoader = elasticLoader(ElasticMethod.Update, client, indexName);
+
+  await startImport(cookie, indexLoader, updateLoader);
+};
+
+const startImport = async <L>(
   cookie: string,
   indexLoader: L,
   updateLoader: L
