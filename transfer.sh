299fa91a8a47b0580f681a084e25a38519a3f88f
FCDave <david.brown@flightcentre.com>
2022-03-17 16:34:58 +1000

    Further instructions in readme Update transfer script to handle mapping transfer

--- transfer.sh
+++ transfer.sh
@@ -6,4 +6,7 @@ OUTPUT_DB="$3"
 
 BATCH_SIZE=1000
 
+# create the index with the same mappings
+elasticdump --input="${INPUT_DB}/${INDEX_NAME}" --output="${OUTPUT_DB}/${INDEX_NAME}" --type=mapping --limit="$BATCH_SIZE"
+# copy the data to the destination index
 elasticdump --input="${INPUT_DB}/${INDEX_NAME}" --output="${OUTPUT_DB}/${INDEX_NAME}" --type=data --limit="$BATCH_SIZE"
